b1844cef97d77a16f6cdb7e30f8691a6ab397e44
George Mbatia <mbatiagithaiga@gmail.com>
2022-07-04 09:48:07 +0300

    Initial commit

---
+++ src/test/java/com/sgasecurity/citmanagermobile/CitmanagermobileApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sgasecurity.citmanagermobile;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CitmanagermobileApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
